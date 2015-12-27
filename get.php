ce87eba526233c5923048005b6d8278fe5ae2b96
trukfit <trukfit@mail.ru>
2015-12-27 12:07:33 +0300

    добавили get file

---
+++ get.php
@@ -0,0 +1,12 @@
+<?php
+    function getName($nam){
+        $name = $nam;
+        echo "Меня зовут $name";
+    }
+getName("Fanit");
+    function setSum($x, $y){
+        $sum = $x + $y;
+        echo $sum;
+    }
+setSum(56, 8);
+?>
