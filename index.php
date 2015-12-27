3dc252afca0f1e31e60ffc2febe7d6c5fac75012
trukfit <trukfit@mail.ru>
2015-12-27 12:32:54 +0300

    Добавили в index.php

--- index.php
+++ index.php
@@ -3,4 +3,10 @@
 		echo "Текущее время и дата - ".date("Y-m-d H:i:s");
 	}
 	getTime();
+   echo "<br>";
+   function mult($x, $y){
+      $sum = $x * $y;
+      echo $sum;
+   }
+   mult(43, 4);
 ?>
